e4efd2ca20d6d5f662e9a8e9af57909dc2c1999d
asalam3312 <sala981013@gmail.com>
2024-09-02 18:01:54 -0500

    update pipfile and pipfile.lock

--- src/api/app.py
+++ src/api/app.py
@@ -9,7 +9,7 @@ from database import db                             # Acá importamos la base de
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 
 # ENCRIPTACION JWT y BCRYPT-------
@@ -47,4 +47,4 @@ with app.app_context():
 
 # AL FINAL ( detecta que encendimos el servidor desde terminal y nos da detalles de los errores )
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
